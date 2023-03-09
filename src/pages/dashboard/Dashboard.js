819780a03c9e58bc0f175eb49ecb33d3f9d68c5b
Yuli Tamara <79260463+Yulitamara@users.noreply.github.com>
2023-03-09 17:13:58 +0200

    comments

--- src/pages/dashboard/Dashboard.js
+++ src/pages/dashboard/Dashboard.js
@@ -1,8 +1,19 @@
-// styles
-import './Dashboard.css'
+import { useCollection } from "../../hooks/useCollection";
 
-import React from "react";
+// components
+import ProjectList from "../../components/ProjectList";
+
+// styles
+import "./Dashboard.css";
 
 export default function Dashboard() {
-  return <div>Dashboard</div>;
+  const { documents, error } = useCollection("projects");
+
+  return (
+    <div>
+      <h2 className="page-title">Dashboard</h2>
+      {error && <p className="error">{error}</p>}
+      {documents && <ProjectList projects={documents} />}
+    </div>
+  );
 }
